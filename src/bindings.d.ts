d68f466444c833497f25c14ed92ce016d8c552ba
Frank Wei <frank@frankwei.xyz>
2021-06-04 17:23:01 +0800

    fix: typing for KV

---
+++ src/bindings.d.ts
@@ -0,0 +1,10 @@
+export {};
+
+declare global {
+  const MY_ENV_VAR: string
+  const MY_SECRET: string
+
+  // Workers KV db
+  const SnippetToKey: KVNamespace
+  const SNIPPETS_STORAGE: KVNamespace
+}
